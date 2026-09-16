d8fbd9c9c4f11e98b88d4c04cb9c2eea197d62e9
Haid117 <leonard.zimnol@proton.me>
2026-09-16 21:11:53 +0200

    added update script

--- shells/.zshrc
+++ shells/.zshrc
@@ -10,11 +10,6 @@ export ZSH="$HOME/.oh-my-zsh"
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="tonotdo"
 
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
 
@@ -81,15 +76,17 @@ source $ZSH/oh-my-zsh.sh
 # export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='nvim'
-# fi
+ if [[ -n $SSH_CONNECTION ]]; then
+   export EDITOR='vim'
+ else
+   export EDITOR='nvim'
+ fi
 
 # Compilation flags
 # export ARCHFLAGS="-arch $(uname -m)"
 
+
+export SUDO_EDITOR=nvim
 
 # Set personal aliases, overriding those provided by Oh My Zsh libs,
 # plugins, and themes. Aliases can be placed here, though Oh My Zsh
@@ -105,4 +102,4 @@ source $ZSH/oh-my-zsh.sh
 alias add="sudo xbps-install"
 alias remove="sudo xbps-remove"
 alias mc="yazi"
-alias query="xbps-query"
+alias qr="xbps-query -Rs"
--- update.sh
+++ update.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 
-confm=/home/$USER/.config/mango
-
+conf=/home/$USER/.config
 yes_no() {
   while true; do
-    read -p "$" " [Y/n] " local yn
+    read -p "$* [y/n]: " yn
     case $yn in
     [Yy]*) return 0 ;;
     [Nn]*)
-      echo "Aborted"
+      echo "Aborted!"
       return 1
       ;;
     esac
   done
-
 }
 
-if ! [ -e README.md ]; then
-  echo "Please run this script from the repository directory!" && return
+if ! pwd | grep "hmc" >/dev/zero; then
+  echo "Please run this script from the repository directory!" && return 0
 fi
 
-if diff -rs mango/ $confm; then
-  echo "Your config files dont differ from the ones in the git repo.\n Maybe check if repo is up to date?" && return
-else
-  yes_no "Warning! Your files in $confm will be overwritten! Proceed?" && echo yay
+diff -rs mango $conf/mango && echo "Your config files dont differ from the ones in the git repo!\n Maybe check if the repo is up to date?" && return 0
+
+if yes_no "Update mango config in "$conf"?" && yes_no "Are you sure you want to overwrite the .config?"; then
+  cp -r ./mango/* "$conf/mango/"
+  cp -r ./foot "$conf/foot"
+  cp -r ./fastfetch "$conf/fastfetch"
+  cp -r ./nvim "$conf/nvim"
+  cp -r ./waybar/ "$cong/mango"
+  cp shells/.zshrc "$HOME/.zshrc"
+
+elif yes_no "Or update the git repo config?" && yes_no "Are you sure you want to overwrite the git repo?"; then
+  cp -r "$conf/mango/*" ./mango/
+  cp -r "$conf/foot/*" ./foot
+  cp -r "$conf/fastfetch/*" ./fastfetch
+  cp -r "$conf/nvim/*"" ./nvim
+  cp "$HOME/.zshrc"" ./shells
 fi
